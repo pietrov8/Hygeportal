04fb10213069c60649b39cc6f99cd82b1372e8fe
Piotr Muszkiewicz <info@piotr-m.pl>
2018-03-29 11:20:24 +0200

    Commit start

---
+++ app/js/vendor/main.js
@@ -0,0 +1,261 @@
+
+$(function () {
+
+    AOS.init({
+        once: true
+    });
+    $(window).on('load', function() {
+        AOS.refresh();
+    });
+    $('.counter').counterUp({
+        delay: 30,
+    });
+
+    initMap();
+
+    $(".slider-inner").addClass("owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000
+    });
+    $(".menu-toggle").on("click", function (e) {
+        e.preventDefault();
+        $(".header__nav").slideToggle();
+    });
+
+    if ($(".slider-img").length) {
+        $(".slider-img").addClass("owl-carousel").owlCarousel({
+            items: 1,
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 5000
+        })
+    }
+    if ($(".gallery").length) {
+        $(".gallery").addClass("owl-carousel").owlCarousel({
+            items: 1,
+            nav: true,
+            // autoplay: true,
+            // autoplayTimeout: 5000
+        })
+    }
+    function slider_popup() {
+        if ($(".house-popup-slider").length) {
+            $(".house-popup-slider").addClass("owl-carousel").owlCarousel({
+                items: 1,
+                nav: true,
+                dots: true,
+                autoplay: true,
+                autoplayTimeout: 5000
+            });
+        }
+    }
+
+
+    $("[data-open-popup]").on("click", function (e) {
+        $("body").addClass("hidde")
+        var content = $($(this).attr("data-open-popup"));
+        content.fadeIn();
+        e.preventDefault();
+        slider_popup();
+        setTimeout(function () {
+            content.find(".owl-prev").css("right", content.find(".owl-dots").outerWidth())
+        }, 100);
+        setTimeout(function () {
+            $(".popup__inner").addClass("open");
+            var window_height = $(window).outerHeight();
+            var content_height = $(".popup__inner").outerHeight();
+
+            if (content_height < window_height) {
+                $(".popup__inner").css("margin-top", (window_height - content_height)/2)
+            } else {
+                $(".popup__inner").css("margin-top", 0)
+            }
+
+        }, 300)
+
+    });
+
+    $(".popup-close").on("click", function (e) {
+        e.preventDefault();
+        $("body").removeClass("hidde")
+        $(".popup").fadeOut();
+        $(".popup__inner").removeClass("open");
+    })
+
+    $(".popup").on("click", function () {
+        $(".popup").fadeOut();
+        $("body").removeClass("hidde")
+    })
+    $(".popup__inner").on("click", function (e) {
+        e.stopPropagation()
+    })
+    $(window).resize(function () {
+        var window_height = $(window).outerHeight();
+        var content_height = $(".popup__inner").outerHeight();
+
+        if (content_height < window_height) {
+            $(".popup__inner").css("margin-top", (window_height - content_height)/2)
+        } else {
+            $(".popup__inner").css("margin-top", 0)
+        }
+    })
+
+    if ($(".multiple-select").length) {
+        $(".multiple-select").multipleSelect({
+            selectAllText: "Wybierz wszystko",
+            allSelected: "Wszystkie",
+            countSelected: "# z % wybrane."
+        })
+    }
+
+    $("body").append("<span class='scroll-top'></span>")
+
+    $(window).bind('scroll', function () {
+        if ($(window).scrollTop() > ($(window).outerHeight()/2)) {
+            $('.scroll-top').addClass("open")
+        } else {
+            $('.scroll-top').removeClass("open")
+        }
+    });
+
+    $('.scroll-to').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - $(".header").outerHeight()
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    $('.scroll-top').on("click", function() {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    $('.select-custom select').customSelect();
+
+
+});
+
+function initMap() {
+    var findMap = document.getElementsByClassName('gmap');
+    var scriptUrl = "http://maps.google.com/maps/api/js" + "?key=AIzaSyCg3z65RVUcKhNs6loK8eoJ8clxBjvfmhM";
+
+    var loadMap = function(){
+        for (var i = findMap.length - 1; i >= 0; i--) {
+            var myMap = findMap[i];
+            var infowindow = null;
+
+            if ($(window).width() > 1170) {
+                var center = new google.maps.LatLng(myMap.getAttribute('data-lat-center'), myMap.getAttribute('data-lon-center'))
+            } else {
+                var center = new google.maps.LatLng(myMap.getAttribute('data-lat'), myMap.getAttribute('data-lon'))
+            }
+
+
+            var map = new google.maps.Map(myMap, {
+                zoom: parseInt(myMap.getAttribute('data-zoom')) || 13,
+                center: center,
+                disableDefaultUI: false,
+                scrollwheel: false
+            });
+
+            var markerOptions = {
+                map: map,
+                icon: 'images/marker.png',
+                animation: google.maps.Animation.DROP,
+                position: new google.maps.LatLng(myMap.getAttribute('data-lat'), myMap.getAttribute('data-lon'))
+            };
+
+            var marker = new google.maps.Marker(markerOptions);
+
+
+            if ($(".map-legend").length) {
+                // Uzupełnianie mapy markerami
+                var markers = window.places_map.markers;
+                var markers_table = [];
+                $.each(markers, function(i, item) {
+                    var position = new google.maps.LatLng(item.lat, item.lon);
+                    var markerOptions = {
+                        id: item.id,
+                        map: map,
+                        icon: item.markerIcon,
+                        position: position,
+                        title: item.title,
+                        type: item.type,
+                        movies: item.movies,
+                        animation: google.maps.Animation.DROP
+                    };
+                    var gmarker = new google.maps.Marker(markerOptions);
+                    var contentString = item.title;
+
+                    infowindow = new SnazzyInfoWindow({
+                        marker: gmarker,
+                        content: contentString,
+                        closeWhenOthersOpen: true,
+                        showCloseButton: false
+                    });
+                    markers_table.push(gmarker)
+                });
+
+                // Filtrowanie markerów
+                function filter_markers() {
+                    $(".si-float-wrapper").hide()
+                    $(".map-legend__filter [data-filter]").each(function () {
+
+                        var category = $(this).attr("data-filter");
+                        var visible_marker = $(this).hasClass("visible");
+
+                        for (var i in markers_table) {
+                            if (markers_table[i].type == category || category.length === 0) {
+                                if (visible_marker) {
+                                    markers_table[i].setVisible(true);
+                                } else {
+                                    markers_table[i].setVisible(false);
+                                }
+                            }
+                        }
+
+                    })
+
+                }
+                filter_markers();
+
+                $(".map-legend__filter [data-filter]").on("click", function () {
+                    $(this).toggleClass("visible");
+                    filter_markers();
+                })
+
+                $(".show-markers").on("click", function () {
+                    $(".map-legend__filter [data-filter]").addClass("visible")
+                    filter_markers()
+                })
+
+                $(".hide-markers").on("click", function () {
+                    $(".map-legend__filter [data-filter]").removeClass("visible")
+                    filter_markers()
+                })
+            }
+
+
+
+            google.maps.event.addDomListener(window, 'resize', function() {
+                if ($(window).width() > 1170) {
+                    var center = new google.maps.LatLng(myMap.getAttribute('data-lat-center'), myMap.getAttribute('data-lon-center'))
+                } else {
+                    var center = new google.maps.LatLng(myMap.getAttribute('data-lat'), myMap.getAttribute('data-lon'))
+                }
+                map.setCenter(center);
+            });
+        }
+    };
+    loadMap()
+}
